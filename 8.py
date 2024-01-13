4d9707659d943ddce1c5a078786c0f4f258e1544
SK <SK>
2024-01-13 21:29:06 +0530

    modified

--- 8.py
+++ 8.py
@@ -1,9 +1,9 @@
 # reverse a string
 str = input("Enter string") #  string variable  
 print ("The original string  is : ",str)   
-reverse_String = ""  # Empty String  
+reverse_string = ""  # Empty String  
 count = len(str) # Find length of a string and save in count variable  
 while count > 0:   
-    reverse_String += str[ count - 1 ] # save the value of str[count-1] in reverseString  
+    reverse_string += str[ count - 1 ] # save the value of str[count-1] in reverseString  
     count = count - 1 # decrement index  
-print ("The reversed string is : ",reverse_String)# reversed string  -
+print ("The reversed string is : ",reverse_string)# reversed string  -
