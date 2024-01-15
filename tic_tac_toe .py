c13fd1ebf49475fa04299d8cf7fe4febfea1a749
SK <SK>
2024-01-15 14:28:52 +0530

    modified

--- tic_tac_toe .py
+++ tic_tac_toe .py
@@ -1,40 +1,43 @@
 def main():
-    choice = int(input("Enter 1 for Single Player, 2 for Multiplayer: ")) # Choice for single player and multiplayer
-    board = [0,0,0,0,0,0,0,0,0]  # Initialization of board
-    if(choice==1):
-        print("Computer: 0 vs You: X")
-        player_number = int(input("Enter to 1 to play 1st or 2 to play 2nd: ")) # Choice to play first or second
-        for i in range(0,9):
-            if(analyze_board(board)!=0):
-                break
-            # (i+player)%2 to check the turn is of player 1(human) or player 2(computer)
-            elif((i+player_number)%2==0): # computers turn
-                comp_turn(board)
-            else:
-                const_board(board)
-                user1_turn(board)
-    else:
-        for i in range(0,9):
-            if(analyze_board(board)!=0):
-                break
-            # (i+player)%2 to check the turn is of player 1 for player 2
-            if((i+player_number)%2==0): # player 1 turn
-                const_board(board)
-                user1_turn(board)
-            else:
-                const_board(board)
-                user2_turn(board)
+    user_choice = 'y'
+    while (user_choice == 'y'):
+        choice = int(input("Enter 1 for Single Player, 2 for Multiplayer: ")) # Choice for single player and multiplayer
+        board = [0,0,0,0,0,0,0,0,0]  # Initialization of board
+        if(choice==1):
+            print("Computer: 0 vs You: X")
+            player_number = int(input("Enter to 1 to play 1st or 2 to play 2nd: ")) # Choice to play first or second
+            for i in range(0,9):
+                if(analyze_board(board)!=0):
+                    break
+                # (i+player)%2 to check the turn is of player 1(human) or player 2(computer)
+                if((i+player_number)%2==0): # computers turn
+                    comp_turn(board)
+                else:
+                    const_board(board)
+                    user1_turn(board)
+        else:
+            for i in range(0,9):
+                if(analyze_board(board)!=0):
+                    break
+                # (i+player)%2 to check the turn is of player 1 for player 2
+                if((i)%2==0): # player 1 turn
+                    const_board(board)
+                    user1_turn(board)
+                else:
+                    const_board(board)
+                    user2_turn(board)
 
-    result = analyze_board(board)
-    if(result==0):
-        const_board(board)
-        print("Draw")
-    if(result==-1):
-        const_board(board)
-        print("X won")
-    if(result==1):
-        const_board(board)
-        print("O won")
+        result = analyze_board(board)
+        if(result==0):
+            const_board(board)
+            print("Draw")
+        if(result==-1):
+            const_board(board)
+            print("X won")
+        if(result==1):
+            const_board(board)
+            print("O won")
+        user_choice = input("Do you wish to play again(y/n): ").lower()
 
 def const_board(board):
     print("Current state of Board: \n")
@@ -42,7 +45,7 @@ def const_board(board):
         if((i>0) and (i%3==0)):
             print("\n")
         if(board[i]==0):
-            print("_", end=" ")
+            print("_ ", end=" ")
         if(board[i]==-1):
             print("X ", end=" ")
         if(board[i]==1):
@@ -60,7 +63,7 @@ def user1_turn(board):
         board[pos-1] = -1
     
 def user2_turn(board):
-    pos = int(input("Enter X's position from (1,2,3...9): "))
+    pos = int(input("Enter O's position from (1,2,3...9): "))
     # while(board[pos-1]!=0):
     #   print("Wrong Move")
     if(board[pos-1]!=0):
@@ -101,7 +104,7 @@ def minmax(board, player):
         value = -2
         for i in range(0,9):
             if(board[i]==0):
-                board[i]=player
+                board[i] = player
                 score = -minmax(board,player*-1) #min max score
                 board[i] = 0
                 if(score>value):
